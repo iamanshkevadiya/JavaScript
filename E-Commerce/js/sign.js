0b63c414e44e75119ae4ea475c949ed365aed39f
Ansh Kevadiya <146837647+iamanshkevadiya@users.noreply.github.com>
2024-06-28 11:53:13 +0530

    Add files via upload

---
+++ E-Commerce/js/sign.js
@@ -0,0 +1,21 @@
+function sign() {
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("pass").value;
+    let conform = document.getElementById("com_pass").value;
+
+    window.localStorage.setItem("inp_email", email);
+
+    if (email == "" && pass == "") {
+        alert("Please Enter a Email first..!");
+        return;
+    }
+    
+    if (pass == conform) {
+        window.localStorage.setItem("inp_pass", pass);
+        window.localStorage.setItem("conf1", conform);
+        window.location.href = "home.html";
+    }
+    else {
+        alert("please enter vaild password...");
+    }
+}
